96ceb6bc82e5fcfb9c8e1c624f677b5e35383cb1
gershon@goosemoose.com <gershon@goosemoose.com@c0f3411e-3ba8-11de-8c0b-2fbac4a05959>
2009-05-17 21:46:33 +0000

    chxdoc template and build script

---
+++ docs.sh
@@ -0,0 +1,12 @@
+chxdoc -o docs 					\
+--includeOnly=haxegui.* 		\
+--installTemplate=true 			\
+--developer=true 				\
+--showAuthorTags=true			\
+--generateTodoFile=true 		\
+--showTodoTags=true 			\
+--headerText="haxegui"			\
+--title="haxegui"				\
+--subtitle="http://code.google.com/p/haxegui/"	\
+--templateDir=docs/template		\
+output.xml,flash9
